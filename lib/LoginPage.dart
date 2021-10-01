e8526eca2fe88ff0aa042b3bf77b74968803062b
Shivangi Pandey <t4snietshivangi06@gmail.com>
2021-10-01 09:52:49 +0530

    Login Screen

---
+++ lib/LoginPage.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget{
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<LoginPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  Widget build(BuildContext context) {
+     return Scaffold(
+         body: Padding(
+             padding: EdgeInsets.all(20),
+             child: ListView(
+               children: <Widget>[
+                 Container(
+                     alignment: Alignment.center,
+                     padding: EdgeInsets.all(10),
+                     child: Text(
+                       'Login Screen',
+                       style: TextStyle(
+                           color: Colors.blue,
+                           fontWeight: FontWeight.w500,
+                           fontSize: 30),
+                     )),
+                 Container(
+                   padding: EdgeInsets.fromLTRB(0, 60, 0, 0),
+                   child: TextField(
+                     controller: nameController,
+                     decoration: InputDecoration(
+                       border: OutlineInputBorder(),
+                       labelText: 'User Name',
+                     ),
+                   ),
+                 ),
+                 Container(
+                   padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                   child: TextField(
+                     obscureText: true,
+                     controller: passwordController,
+                     decoration: InputDecoration(
+                       border: OutlineInputBorder(),
+                       labelText: 'Password',
+                     ),
+                   ),
+                 ),
+                 MaterialButton(
+                   onPressed: (){
+                     //forgot password screen
+                   },
+                   textColor: Colors.blue,
+                   child: Text('Forgot Password'),
+                 ),
+                 Container(
+                     height: 50,
+                     padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                     child: ElevatedButton(
+                       child: Text('Login'),
+                       onPressed: () {
+                         print(nameController.text);
+                         print(passwordController.text);
+                       },
+                     )),
+                 Container(
+                     child: Row(
+                       children: <Widget>[
+                         Text('Does not have account?'),
+                         MaterialButton(
+                           textColor: Colors.blue,
+                           child: Text(
+                             'Sign Up',
+                             style: TextStyle(fontSize: 20),
+                           ),
+                           onPressed: () {
+                             //signup screen
+                           },
+                         )
+                       ],
+                       mainAxisAlignment: MainAxisAlignment.center,
+                     ))
+               ],
+             )));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:learning_flutter/LoginPage.dart';
 
+void main()=>runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
